c203bc693e5febeab5271e291f112114a0da46d1
Kieran T <kieran123416@gmail.com>
2023-09-26 20:55:44 +0100

    Framework Pushed
    Not ready to use

---
+++ Source/VR_Template/Player/Base_VR_Character.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Base_VR_Character.generated.h"
+
+UCLASS()
+class VR_TEMPLATE_API ABase_VR_Character : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ABase_VR_Character();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
